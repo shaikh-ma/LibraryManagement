c9b33458c87307633fdc87caa85e906da2cf598b
shaikh-ma <shaikhaamir101996@gmail.com>
2024-03-31 03:09:07 +0530

    Making more updates

--- libman/library/models.py
+++ libman/library/models.py
@@ -3,10 +3,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Book(models.Model):
+    book_id = models.IntegerField(primary_key = True)
+    issued_to = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     is_available = models.BooleanField(default=True)
-    user_issued = models.ForeignKey(User, on_delete=models.CASCADE)
+    return_date = models.DateField(blank=True, null=True)
+    issue_date = models.DateField(blank=True, null=True)
     
     def __str__(self):
         return self.title
--- libman/users/forms.py
+++ libman/users/forms.py
@@ -2,10 +2,27 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+CHOICE_YEAR =(
+("1", "First Year"),
+("2", "Second Year"),
+("3", "Third Year"),
+("4", "Other"),
+)
 
 class UserRegisterForm(UserCreationForm):
+    roll_no = forms.IntegerField()
+    icard_no = forms.IntegerField()
+    class_div = forms.ChoiceField(choices=CHOICE_YEAR)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = [
+            'username', 
+            'roll_no',
+            'icard_no',
+            'class_div',
+            'email', 
+            'password1', 
+            'password2'
+            ]
