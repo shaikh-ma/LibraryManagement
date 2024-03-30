82d378750597855115161ee7a1382ca39ca16aa1
shaikh-ma <shaikhaamir101996@gmail.com>
2024-03-31 02:03:13 +0530

    Making updates

--- libman/library/models.py
+++ libman/library/models.py
@@ -1,22 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    # issued_to = models.CharField(max_length=200)
-    number_of_copies = models.IntegerField(default=0)
-    available_copies = models.IntegerField(default=0)
-
+    is_available = models.BooleanField(default=True)
+    user_issued = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.title
 
-
-# class Borrower(models.Model):
-#     # bookname = models.ForeignKey(Book, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=200)
-#     email = models.EmailField()
-#     issue_date = models.DateTimeField()
-#     return_date = models.DateTimeField()
-
-#     def __str__(self):
-#         return self.name
--- libman/library/views.py
+++ libman/library/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Book
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'library/index.html'
     context_object_name = 'books'
