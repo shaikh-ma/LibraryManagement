be996fceeda9ad464d7fc1e86a92b3316c58213a
shaikh-ma <shaikhaamir101996@gmail.com>
2024-03-31 22:34:26 +0530

    Adding list for my books

--- libman/library/urls.py
+++ libman/library/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import BookListView
-from . import views
+from .views import BookListView, UserBooksListView
 
 urlpatterns = [
     path("", BookListView.as_view(), name="home"),
+    path('user/<str:username>', UserBooksListView.as_view(), name='user-books'),
 ]
--- libman/library/views.py
+++ libman/library/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Book
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'library/home.html'
     context_object_name = 'books'
+
+
+class UserBooksListView(ListView):
+    model = Book
+    template_name = 'library/user_books.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'books'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Book.objects.filter(issued_to=user)
