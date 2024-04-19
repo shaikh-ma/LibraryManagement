7b364395a846743fc7c9cc528417135656a4684f
shaikh-ma <shaikhaamir101996@gmail.com>
2024-04-20 01:39:06 +0530

    more updates

---
+++ libman/library/static/library/js/search_books.js
@@ -0,0 +1,17 @@
+function findBook() {
+search_for = $('#searchbook').val().toLowerCase();
+titles = $('.card-title');
+bookcards = $('.card');
+
+for (let i=0; i<titles.length; i++){
+  title = titles[i].innerText.toLowerCase();
+  match = title.indexOf(search_for);
+  if (match > -1){
+    bookcards[i].style.display = "block"
+  }
+  else{
+        bookcards[i].style.display = "none"
+  }
+}
+
+}
